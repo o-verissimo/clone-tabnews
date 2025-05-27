1daf658a0ad1e49534ca50dcf6c28c8e22b2010d
Paulo Pereira <102670336+o-verissimo@users.noreply.github.com>
2025-05-27 23:57:00 +0000

    Foi concluido o dia 4

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() 
+{
+    return <h1>Teste</h1>
+}
+
+export default Home; //com o default podemos dizer
+// que a função inicial para poder chamar as
+// outras é esta que foi declarada
+

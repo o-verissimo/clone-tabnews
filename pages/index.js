63989a15d4df634a71533f0c5372a394969b1749
Paulo Pereira <102670336+o-verissimo@users.noreply.github.com>
2025-12-04 00:13:20 +0000

    Correção de frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Eu amo amar você mesmo você não tomando banho, 717 dias.</h1>
+    return <h1>Eu amo amar você mesmo você não tomando banho. 717 dias.</h1>
 }
 
 export default Home;
